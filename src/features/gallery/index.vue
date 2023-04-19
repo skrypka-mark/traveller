4167e9705170e6caa0b84771d3d6b9d6ad8f39ba
Mark Skrypka <mark.skrypka@gmail.com>
2023-04-20 00:34:10 +0200

    little fix

--- src/features/gallery/index.vue
+++ src/features/gallery/index.vue
@@ -24,48 +24,28 @@ const selectedCountryId = computed(() => route.query.countryId);
 const openCountryDetails = (id: string) => {
     router.push({ hash: `#gallery`, query: { countryId: id } });
 };
-const ae = () => {
-    transition.value = false;
-};
-const bl = () => {
-    transition.value = true;
-};
 </script>
 
 <template>
     <Section id='gallery' :style='{ paddingInline: 0 }'>
         <Blur :blur=10 />
         <div :class='$style[`bg-darken`]' />
-        <Transition name='fade' mode='out-in' @after-enter=ae @before-leave=bl>
-            <div :class='$style[`gallery-container`]' v-if=!selectedCountryId>
-                <Title>Country tours</Title>
-                <ul :class='$style[`gallery-list`]'>
-                    <div :class='[$style[`arrow-container`], $style.left]'>
-                        <ArrowLeft :class='$style.arrow' />
-                    </div>
-                    <li :key=country v-for='{ id, country, image } in countryTours' :class='$style[`gallery-list__item`]' v-shared-element:[image]>
-                        <CountryImage :image=image :title=country @click=openCountryDetails(id) />
-                    </li>
-                    <div :class='[$style[`arrow-container`], $style.right]'>
-                        <ArrowRight class='arrow' />
-                    </div>
-                </ul>
-            </div>
-            <CountryDetails :id=selectedCountryId v-else />
-        </Transition>
+        <div :class='$style[`gallery-container`]' v-if=!selectedCountryId>
+            <Title>Country tours</Title>
+            <ul :class='$style[`gallery-list`]'>
+                <div :class='[$style[`arrow-container`], $style.left]'>
+                    <ArrowLeft :class='$style.arrow' />
+                </div>
+                <li :key=country v-for='{ id, country, image } in countryTours' :class='$style[`gallery-list__item`]' v-shared-element:[image]>
+                    <CountryImage :image=image :title=country @click=openCountryDetails(id) />
+                </li>
+                <div :class='[$style[`arrow-container`], $style.right]'>
+                    <ArrowRight class='arrow' />
+                </div>
+            </ul>
+        </div>
+        <CountryDetails :id=selectedCountryId v-else />
     </Section>
 </template>
-
-<style lang='scss'>
-.fade-enter-active,
-.fade-leave-active {
-    transition: all .2s ease-out;
-}
-
-.fade-enter-from,
-.fade-leave-to {
-    // opacity: 0;
-}
-</style>
 
 <style lang='scss' module src='./styles.module.scss' />
